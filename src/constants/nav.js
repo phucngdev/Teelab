27408d01ab0c5612c2a008a33587a3b11a69bb06
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-03-24 21:12:02 +0700

    refactor: path router

---
+++ src/constants/nav.js
@@ -0,0 +1,42 @@
+export const navList = [
+  {
+    text: "Tất cả sản phẩm",
+    link: "/tat-ca-san-pham",
+  },
+  {
+    text: "Áo thun",
+    link: "/ao-thun",
+  },
+  {
+    text: "Baby Tee",
+    link: "/baby-tee",
+  },
+  {
+    text: "Áo polo",
+    link: "/ao-polo",
+  },
+  {
+    text: "Áo sơ mi",
+    link: "/ao-so-mi",
+  },
+  {
+    text: "Áo khoác",
+    link: "/ao-khoac",
+  },
+  {
+    text: "Hoodie",
+    link: "/hoodie",
+  },
+  {
+    text: "Quần",
+    link: "/quan",
+  },
+  {
+    text: "Quần nữ",
+    link: "/quan-nu",
+  },
+  {
+    text: "Phụ kiện",
+    link: "/phu-kien",
+  },
+];
---
+++ src/constants/path.js
@@ -0,0 +1,25 @@
+import logo from "../../public/logo.png";
+export const home = {
+  path: "/",
+  text: "TRANG CHỦ",
+};
+export const service = {
+  path: "/chinh-sach-doi-tra",
+  text: "CHÍNH SÁCH ĐỔI TRẢ",
+};
+export const tableSize = {
+  path: "/bang-size",
+  text: "BẢNG SIZE",
+};
+export const system = {
+  path: "/he-thong-cua-hang",
+  text: "HỆ THỐNG CỦA HÀNG",
+};
+export const logoCenter = {
+  path: "/",
+  url: logo,
+};
+export const checkOrder = {
+  path: "/kiem-tra-don-hang",
+  text: "Kiểm tra đơn hàng",
+};
